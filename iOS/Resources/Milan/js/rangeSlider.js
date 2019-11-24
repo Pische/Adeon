3886ecd9795c0d80ed7b72d34a0546271c76ab1c
Pische <pischetolavincenzo@hotmail.it>
2019-11-24 16:04:23 +0100

    Range Slider finito

--- iOS/Resources/Milan/js/rangeSlider.js
+++ iOS/Resources/Milan/js/rangeSlider.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
-    /* -6 sono i px di scarto dei lati dello sfondo "range.png" */
-    var distanzaSinistra = ($('.ui-slider-handle').offset().left - 6) + "px";
+    /* -5 sono i px di scarto dei lati dello sfondo "range.png" */
+    var distanzaLeft = ($('.ui-slider-handle').offset().left - 5) + "px";
 
-    $(".ui-slider-popup").css("left", distanzaSinistra);
+    $(".ui-slider-popup").css("left", distanzaLeft);
 });
